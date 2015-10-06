1c03adc0dbcb6373b6a8e0f21924d77a110e3b21
Manuel Lima <mlima@despegar.com>
2015-10-05 22:34:33 -0300

    New datasource professional connection pool

--- src/main/webapp/app/view/process/ProcessList.js
+++ src/main/webapp/app/view/process/ProcessList.js
@@ -7,15 +7,22 @@ Ext.define('App.view.process.ProcessList', {
     autoScroll: true,
     
     store: 'ProcessStore',
-    columns: [
-        { text: 'C&oacute;digo',  dataIndex: 'id' },
-        { text: 'Inicio', dataIndex: 'startDate', flex: 1 },
-        { text: 'Iniciado por', dataIndex: 'startedBy', flex: 1 },
-        { text: 'Corresponde a', dataIndex: 'valuesFrom', flex: 1 },
-        { text: 'Enviados', dataIndex: 'sentValues', flex: 1 },
-        { text: 'Estado', dataIndex: 'status', flex: 1 },
-        { text: 'Resultado', dataIndex: 'finalStatus', flex: 1 }
-    ],
+    columns: {
+		defaults : {
+    		draggable: false,
+    		sortable: false,
+    		hideable: false
+    	},
+    	items: [
+	        { text: 'C&oacute;digo',  dataIndex: 'id' },
+	        { text: 'Inicio', dataIndex: 'startDate', flex: 1 },
+	        { text: 'Iniciado por', dataIndex: 'startedBy', flex: 1 },
+	        { text: 'Corresponde a', dataIndex: 'valuesFrom', flex: 1 },
+	        { text: 'Enviados', dataIndex: 'sentValues', flex: 1 },
+	        { text: 'Estado', dataIndex: 'status', flex: 1 },
+	        { text: 'Resultado', dataIndex: 'finalStatus', flex: 1 }
+	    ]
+    },
     autoRender: true,
     tbar: {
     	defaults: {
